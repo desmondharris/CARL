45269232d472509f545d2fd8d9aa1a9b78760740
Desmond Harris <desmondktharris@gmail.com>
2024-02-29 17:32:43 -0500

    fix font and rendering issues

--- trial/display_trial.py
+++ trial/display_trial.py
@@ -1,20 +1,23 @@
 import tkinter as tk
+from tkinter import font
 from pylsl import StreamInlet, StreamOutlet, StreamInfo, resolve_stream
 import threading
 from time import sleep, time
+import sys
 
-DEBUG = 1
-
+DEBUG = 0
+TK_FNT_SIZE = 32
 TRIAL_TYPES = ["UNI_RIGHT", "UNI_LEFT", "BI"]
-TRIAL_TYPE = TRIAL_TYPES[0]
-OUTPUT_STREAM_NAME = "curl_relax_markers"
+TRIAL_TYPE = sys.argv[1]
+OUTPUT_STREAM_NAME = "TRIAL_OUTP_" + TRIAL_TYPE
 TIME_PER_LABEL = 3.5
-N_SAMPLES = 10
+N_SAMPLES = 2
 LABELS = {
     "UNI_RIGHT": ["RR", "RL"],
     "UNI_LEFT": ["LR", "LL"],
     "BI": ["BR", "BL"]
 }
+
 
 
 def send_test_streams(outlet_name):
@@ -33,6 +36,7 @@ def send_test_streams(outlet_name):
 class LSLApp:
     def __init__(self):
         self.root = tk.Tk()
+        self.root.attributes("-fullscreen", True)
         self.root.title("LSL Application")
         # Set a default window size (optional)
         self.root.geometry('800x600')
@@ -41,8 +45,17 @@ class LSLApp:
         # Add two columns labeled L and R
         self.display_frame = tk.Frame(self.root)
         self.display_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
-        self.display_label = tk.Label(self.display_frame, text="LEFT:    RIGHT:         ")
-        self.display_label.pack()
+        self.r_frm = tk.Frame(self.display_frame)
+        tk.Label(self.r_frm, text="\n\nRight Hand:\n\n",font = font.Font(family="Helvetica", size=TK_FNT_SIZE)).pack()
+        self.right_hand_label = tk.Label(self.r_frm, text="RELAX", font = font.Font(family="Helvetica", size=TK_FNT_SIZE))
+        self.right_hand_label.pack()
+        self.r_frm.pack(side=tk.RIGHT, fill=tk.BOTH, expand=True)
+        self.l_frm = tk.Frame(self.display_frame)
+        tk.Label(self.l_frm, text="\n\nLeft Hand:\n\n",font = font.Font(family="Helvetica", size=TK_FNT_SIZE)).pack()
+        self.left_hand_label = tk.Label(self.l_frm, text="RELAX", font = font.Font(family="Helvetica", size=TK_FNT_SIZE))
+        self.left_hand_label.pack()
+        self.l_frm.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+        self.display_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
 
         # Create LSL outstream
         info = StreamInfo(name=OUTPUT_STREAM_NAME, type='Markers', channel_count=1, nominal_srate=0,
@@ -55,13 +68,20 @@ class LSLApp:
         self.outlet.push_sample(['calib-begin'])
         for _ in range(N_SAMPLES):
             choice = int(time() * 10) % 2
-            symbol_dict = {
-                "RL": "Right hand:  Rotate left  \n\n Left hand: RELAX",
-                "RR": "Right hand:  Rotate right \n\n Left hand: RELAX",
-                "LR": "Right hand:  RELAX\n\n Left hand: Rotate right",
-                "LL": "Right hand:  RELAX\n\n Left hand: Rotate left",
-            }
-            self.display_label.config(text=symbol_dict[LABELS[TRIAL_TYPE][choice]])
+
+            # hand is first symbol, direction is second symbol
+            hand, direction = LABELS[TRIAL_TYPE][choice][0], LABELS[TRIAL_TYPE][choice][1]
+            # CHANGE SYMBOLS HERE
+            if hand == "R":
+                self.right_hand_label.config(text="==>" if direction == "R" else "<==")
+                self.left_hand_label.config(text="RELAX")
+            elif hand == "L":
+                self.left_hand_label.config(text="==>" if direction == "R" else "<==")
+                self.right_hand_label.config(text="RELAX")
+            elif hand == "B":
+                dir = " ==> " if direction == "R" else " <== "
+                self.left_hand_label.config(text=dir)
+                self.right_hand_label.config(text=dir)
             self.outlet.push_sample([LABELS[TRIAL_TYPE][choice]])
             sleep(TIME_PER_LABEL)
         self.outlet.push_sample(['calib-end'])
@@ -81,7 +101,7 @@ class LSLApp:
 
         # Clean up
         print("Exiting...")
-        self.outlet.push_sample(['calib-end'])
+
 
 
 app = LSLApp()
