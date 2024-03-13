509410d191627907a8beef00c238d9f75d62a7f9
Desmond Harris <desmondktharris@gmail.com>
2024-03-13 16:52:23 -0400

    add wait for last trial

--- trial/run3trials.ps1
+++ trial/run3trials.ps1
@@ -19,20 +19,16 @@ echo "Collecting second run..."
 Start-Process powershell.exe -ArgumentList  "cd C:\Users\desmo\Desktop\CARL\trial; python display_trial.py UNI_LEFT"
 
 Read-Host -Prompt "Press any key to launch Trial 3"
-
 echo "Launching Trial 3: Bilateral" "Connect EEG, open Lab Recorder, and wait for the stream TRIAL_OUTP_BI..."
-
-
 echo "Collecting first run..."
 Start-Process powershell.exe -ArgumentList  "cd C:\Users\desmo\Desktop\CARL\trial; python display_trial.py BI-ONEHAND"
 Read-Host -Prompt "Press any key to launch next run (don't forget to record to a new .xdf!)"
 echo "Collecting second run..."
-
 Start-Process powershell.exe -ArgumentList  "cd C:\Users\desmo\Desktop\CARL\trial; python display_trial.py BI-ONEHAND"
 echo "Experiment complete!"
 
-
-echo "Launching Trial 3: Bilateral Two Hand" "Connect EEG, open Lab Recorder, and wait for the stream TRIAL_OUTP_BI..."
+Read-Host -Prompt "Press any key to launch trial 4"
+echo "Launching Trial 4: Bilateral Two Hand" "Connect EEG, open Lab Recorder, and wait for the stream TRIAL_OUTP_BI..."
 echo "Collecting first run..."
 Start-Process powershell.exe -ArgumentList  "cd C:\Users\desmo\Desktop\CARL\trial; python display_trial.py BI-TWOHAND"
 Read-Host -Prompt "Press any key to launch next run (don't forget to record to a new .xdf!)"
